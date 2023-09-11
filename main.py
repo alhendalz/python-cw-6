a90f1c163d52db69d7f70efabe54c80c97e967ce
alhendalz <142683927+alhendalz@users.noreply.github.com>
2023-09-11 06:56:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,18 @@
 person = {
     "name" : "zahraa" ,
     "age" : 18 ,
-    "hoppies" : ["shopping" , "dancing" , "traveling"]
+    "hobbies" : ["shopping" , "dancing" , "football"]
 }
 print(person["name"])
 print(person["age"])
 
 person["age"]=19
 person["country"]="uk"
-print.len(person)
+print(person)
+print(f"number of objects in dictionry: {len((person))}")
+person["hobbies"].append("coding")
+
+def check_hobbies(person):
+    if len(person["hobbies"])>=3:
+        return print("wow you are amazing!")
+check_hobbies(person)
