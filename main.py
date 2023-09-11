82a451d4da47715aee6b18fb662c616471065ec8
alhendalz <142683927+alhendalz@users.noreply.github.com>
2023-09-11 06:21:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # write your code here
+person = {
+    "name" : "zahraa" ,
+    "age" : 18 ,
+    "hoppies" : ["shopping" , "dancing" , "traveling"]
+}
+print(person["name"])
+print(person["age"])
+
+person["age"]=19
+person["country"]="uk"
+print.len(person)
